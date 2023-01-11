fe0ad1462731e0a0267a36c772da4b06dee2c477
Azhe <1220443121@qq.com>
2023-01-11 20:00:52 +0800

    first commit

---
+++ src/main/java/com/azhe/test.java
@@ -0,0 +1,13 @@
+package com.azhe;
+
+/**
+ * @Description
+ * @auther 任志哲
+ * @create: 2023-01-11 19:55
+ * 拒绝内卷，从我做起
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
