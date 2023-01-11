3b1dfa13de20480958931dd7538f17cff369887e
Azhe <1220443121@qq.com>
2023-01-11 20:13:23 +0800

    hot-fix commit1

--- src/main/java/com/azhe/test.java
+++ src/main/java/com/azhe/test.java
@@ -11,5 +11,6 @@ public class test {
         System.out.println("hello");
         System.out.println("hello git");
         System.out.println("hello git!");
+        System.out.println("hello git!!!");
     }
 }
